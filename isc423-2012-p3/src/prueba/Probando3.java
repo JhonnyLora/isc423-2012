81e44f0e5d530dfbb9016b487cde7503f33f8d2b
jhonnytunes <jhonnytunes@jhonnytunespc>
2012-07-11 20:46:59 -0400

    Probando .av

---
+++ isc423-2012-p3/src/prueba/Probando3.java
@@ -0,0 +1,20 @@
+package prueba;
+/**
+ * 
+ */
+
+/**
+ * @author jhonnytunes
+ *
+ */
+public class Probando3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
